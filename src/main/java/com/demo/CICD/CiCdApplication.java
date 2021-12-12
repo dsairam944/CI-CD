cd1e271aa4034fdc94f02c1c84b08b0e16fcd4a3
dsairam944 <dsairam944@gmail.com>
2021-12-12 16:35:54 -0500

    added log statement

--- src/main/java/com/demo/CICD/CiCdApplication.java
+++ src/main/java/com/demo/CICD/CiCdApplication.java
@@ -3,11 +3,13 @@ package com.demo.CICD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class CiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdApplication.class, args);
+		System.out.println("Hello sai, You are in your main class");
 	}
-
 }
