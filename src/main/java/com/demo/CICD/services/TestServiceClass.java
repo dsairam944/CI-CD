dca50091b25c4e0d8cbea58bf92ec68fc925eba6
Sai Ram Doddapaneni <49488390+dsairam944@users.noreply.github.com>
2021-12-12 16:50:01 -0500

    Update TestServiceClass.java

--- src/main/java/com/demo/CICD/services/TestServiceClass.java
+++ src/main/java/com/demo/CICD/services/TestServiceClass.java
@@ -1,4 +1,7 @@
 package com.demo.CICD.services;
 
 public class TestServiceClass {
+  	public static void main(String[] args) {
+		System.out.println("Hello sai, You are in your main class");
+	}
 }
